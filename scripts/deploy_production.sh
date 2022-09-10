9b184b67937dabe4177c424bc2094329b4c38b37
Touré Holder <toureh@hotmail.com>
2022-09-10 19:22:30 -0300

    chore: migrate ci/cd to github actions

--- scripts/deploy_production.sh
+++
@@ -1,5 +0,0 @@
-echo "###### Starting deployment to production ######"
-
-npm install -g firebase-tools
-
-firebase deploy --only hosting:production --token "$FIREBASE_TOKEN"
--- scripts/deploy_staging.sh
+++
@@ -1,5 +0,0 @@
-echo "###### Starting deployment to staging ######"
-
-npm install -g firebase-tools
-
-firebase deploy --only hosting:staging --token "$FIREBASE_TOKEN"
